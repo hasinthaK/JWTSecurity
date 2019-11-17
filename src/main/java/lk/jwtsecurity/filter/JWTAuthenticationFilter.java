cb2c14458b3bc20ebcc474387ce312da09b38a78
hasinthaK <hasiroxsola@gmail.com>
2019-11-17 12:38:02 +0530

    JWT is now returned as json in the res body

--- src/main/java/lk/jwtsecurity/filter/JWTAuthenticationFilter.java
+++ src/main/java/lk/jwtsecurity/filter/JWTAuthenticationFilter.java
@@ -19,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -57,8 +58,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .sign(Algorithm.HMAC512(JWTProperties.SECRET.getBytes()));
 
         log.info("JWT created and returned -> " + JWTProperties.TOKEN_PREFIX + token);
+
+// add token as json body of the response
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(JWTProperties.TOKEN_PREFIX + token);
+        out.flush();
+
         response.addHeader(JWTProperties.HEADER_STRING, JWTProperties.TOKEN_PREFIX + token);
 //        response. ("token: " + JWTProperties.TOKEN_PREFIX + " " + token);
+
 
     }
 
